92faed09c92c30568cd244f3a99e9cfacd996fef
zero <zero@zero-300E4C-300E5C-300E7C.(none)>
2013-11-18 01:13:04 -0400

    .

--- js/ProviderList.js
+++ js/ProviderList.js
@@ -13,22 +13,26 @@ function getlistofservices(Category, latitude, longitude, positions){
     alert(" Estado: "+ State + " City: "+ City + " Country: "+ Country);
     $.ajax({
         type: "GET",
-        url : "http://eaa.ece.uprm.edu:3000/mobileEmergency?City=" + City,
+        url : "http://eaa.ece.uprm.edu:3000/mobileEmergency?city=" + City,
         contentType: "application/json;  charset=ISO-8859-15",
         dataType: "json",
         data: {category: Category, latitude: latitude, longitude:longitude, state: "PR", country:"PR", clientPhoneNumber: "787-472-9078"},
         success : function(data){
             alert("Entro a Ajax");
+
             for(var i = 0; i < 1; i++){
 
                 if (Category == 'hospital'){
+                    alert("Hospital");
+
                     positions.savePosition(
                         new Coords(
                             data[i].latitude,
                             data[i].longitude,
-                            positions.position[i].accuracy
-                        ), Country ,State ,City ,data[i].mobile);
-                    alert("Hospital latitud: "+ data[i].latitude+ " Longituded: "+ data[i].longitude);
+                            position[i].accuracy
+                        ), Country ,State ,City , '7873627434' );
+                    alert("Hospital latitud: "+ data[0].latitude + " Longitude: "+ data[0].longitude);
+
                 }
                 else {
                     positions.savePosition(
