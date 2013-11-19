064ca3196e0e28356320c7eda501b73f698f99f8
zero <zero@zero-300E4C-300E5C-300E7C.(none)>
2013-11-19 13:29:16 -0400

    Probando algo de Marvin

--- js/ProviderList.js
+++ js/ProviderList.js
@@ -15,7 +15,7 @@ function getlistofservices(Category, latitude, longitude){
     $.ajax({
         type: "GET",
         url : "http://eaa.ece.uprm.edu:3000/mobileEmergency?city=" + City,
-        contentType: "application/json; charset=utf-8",
+        contentType: "application/json; charset=ISO-8859-1 ",
         dataType: "json",
         data: {category: Category, latitude: latitude, longitude:longitude, state: "PR", country:"PR", clientPhoneNumber: "787-472-9078"},
         success : function(data){
