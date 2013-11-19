a82244553fa705482c57c90887f263772bc065d9
zero <zero@zero-300E4C-300E5C-300E7C.(none)>
2013-11-18 22:43:41 -0400

    .

--- js/ProviderList.js
+++ js/ProviderList.js
@@ -16,8 +16,7 @@ function getlistofservices(Category, latitude, longitude){
         type: "GET",
         url : "http://eaa.ece.uprm.edu:3000/mobileEmergency?city=" + City,
         contentType: "application/json; charset=uff-8",
-        dataType: "jsonp",
-        crossDomain: true,
+        dataType: "json",
         data: {category: Category, latitude: latitude, longitude:longitude, state: "PR", country:"PR", clientPhoneNumber: "787-472-9078"},
         success : function(data){
             alert("Entro a Ajax: "+ data);
